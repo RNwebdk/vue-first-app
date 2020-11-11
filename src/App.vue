7286ac202a8a99c686f0d3c90b0f9394f6933133
Rami <cashamir@gmail.com>
2020-11-11 23:22:38 +0100

    Adding a Create Friend component and passing data to parent

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <header>
       <h1>My Friends</h1>
     </header>
+      <new-friend
+      @add-contact="addContact"
+      ></new-friend>
     <ul>
         <friend-contact
         v-for="friend in friends"
@@ -44,6 +47,16 @@
       toggleFavoriteStatus(friendId){
         const identifiedFriend = this.friends.find(friend => friend.id === friendId);
         identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+      },
+      addContact(name, phone, email){
+        const newFriendContact = {
+          id: new Date().toISOString(),
+          name: name,
+          phone: phone,
+          email: email,
+          isFarvorite: false,
+        };
+        this.friends.push(newFriendContact);
       }
     }
   }
@@ -82,7 +95,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form  {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -116,4 +130,19 @@ header {
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
 
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
+}
 </style>
